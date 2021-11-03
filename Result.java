ff37895989b7b88b9a049f5dec9e188a8699881f
Kelsey Katie Sara Anethesia <pcvoxels@gmail.com>
2021-11-03 16:08:06 +0800

    idek wtf these fiels are xd

---
+++ Result.java
@@ -0,0 +1,18 @@
+class Result {
+    public static void main(String[] args)
+
+     {
+
+        int x = 1, square = 0;
+
+        while (x <= 3){
+            square = x * x;
+            System.out.println(square);
+            x++;
+        }
+   
+             
+   
+             
+     }
+}
